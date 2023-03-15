cb579b7bf06e17be121e5ff2e3384c0c01aa2a3a
joaosantosvictor <joaovictormedia@gmail.com>
2023-03-15 16:29:24 -0300

    var-ex01

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+// const nome = prompt("Qual é o seu nome?")
+// //tipo string 
+// const idade = Number(prompt("Quantos anos você tem?"))
+// //tipo number, dessa forma para fazer o casting para number
+// console.log(nome)
+// console.log(idade)
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// console.log("Olá,", nome, "você tem", idade, "anos.")
+
+let respostaA = confirm("Já fez faculdade?")
+let respostaB = confirm("Vc tem carro?")
+let respostaC = confirm("Quer um dindin?")
+
+console.log("Você fez faculdade?", respostaA ,"Você tem carro?", respostaB, "Quer um dindin?", respostaC)
+
+const perguntaA = "Já fez faculdade?"
+const perguntaB = "Vc tem carro?"
+const perguntaC = "Quer um dindin?"
+
+console.log(perguntaA, respostaA )
+console.log(perguntaB, respostaB)
+console.log(perguntaC, respostaC)
